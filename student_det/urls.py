80bb1320a5b5681f77372409f04aecd82b6bb365
Supun <supundayan23@gmail.com>
2023-01-18 15:47:22 +0530

    Added search student by uni and degree

--- student_det/urls.py
+++ student_det/urls.py
@@ -5,9 +5,6 @@ urlpatterns = [
     path('', views.student_det_list),
     path('skills', views.skill_list),
     path('pro_langs', views.pro_lang_list),
-<<<<<<< HEAD
     path('search/<str:degree>,<str:uni>', views.search_student),
-=======
     path('<int:id>',views.student_det_detail)
->>>>>>> 14d71b395843e774b5f7d740accc2e8e781e7e8f
 ]
