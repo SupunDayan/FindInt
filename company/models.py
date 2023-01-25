4f175cdb7cc01de803dd0dfa42bcb0f468e1c6c9
Nethsara <chaminethdu@gmail.com>
2023-01-25 00:09:01 -0800

    Did some modifications on both company and internship app models

--- company/models.py
+++ company/models.py
@@ -4,5 +4,8 @@ class Company(models.Model):
     name = models.CharField(max_length=100, default="")
     description = models.CharField(max_length=5000, default="")
     email  = models.EmailField()
+
+    def __str__(self):
+        return self.name + ' - ' + self.description
     
    
--- internship/models.py
+++ internship/models.py
@@ -9,16 +9,24 @@ class Internship(models.Model):
     open_date = models.DateField(null=True)
     closing_date = models.DateField(null=True)
 
+    def __str__(self):
+        return self.title
+    
+
 
 class Skill(models.Model):
     skill = models.CharField(max_length=100)
     Internship =  models.ForeignKey(Internship, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.skill
 
 class ProLang(models.Model):
     ProLang = models.CharField(max_length=100)
     Internship =  models.ForeignKey(Internship, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.ProLang
 
 
 
